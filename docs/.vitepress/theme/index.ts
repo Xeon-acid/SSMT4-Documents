15b6665b551a5b49aec501a1558d2a5ae51b6374
Perxenic Acid <angeloyrd856@gmail.com>
2026-02-12 20:48:36 +0800

    op

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,22 +3,18 @@ import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
 import 'katex/dist/katex.min.css'
+import './custom.css'
 
 import DarkWatcher from '../components/DarkWatcher.vue'
 
-// export default {
-//   ...DefaultTheme,
-//   enhanceApp({ app }) {
-//     app.component('DarkWatcher', DarkWatcher)
-//   }
-// }
-
-
 export default {
     extends: DefaultTheme,
-    enhanceApp({ app }) {
-        enhanceAppWithTabs(app)
-        app.component('DarkWatcher', DarkWatcher)
+
+    enhanceApp(ctx) {
+        // tabs 插件
+        enhanceAppWithTabs(ctx.app)
+
+        // 自定义组件
+        ctx.app.component('DarkWatcher', DarkWatcher)
     },
 } satisfies Theme
-
